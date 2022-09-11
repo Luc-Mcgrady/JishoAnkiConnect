4e6fee52997708b0296f83f03556732028fb6ba3
Luc Mcgrady <lucmcgrady@gmail.com>
2022-09-11 03:36:15 +0100

    Fix: Rate limiting

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const example = 
-`
-https://jisho.org/search/%E9%AD%85%E5%8A%9B
-https://jisho.org/search/%E4%B8%8D%E6%80%9D%E8%AD%B0
-https://jisho.org/search/%E5%85%B1%E6%84%9F
-https://jisho.org/search/%E6%B8%A9%E3%81%8B%E3%81%95
-`
-
 const urls = example.split("\n").filter(a=>a!="")
 
-async function getUrl(url) {
+function delay(t = delay_ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, t);
+    });
+}
+
+async function getUrl(url, i) {
     const slug = url.match(/https:\/\/jisho\.org\/search\/([%A-F0-9]+)$/)
 
     if (!slug)
         return
 
-    const resp = await fetch(`https://jisho.org/api/v1/search/words?keyword=${slug}`)
-    const full = await resp.json()
-    const main = full.data[0]
-    return `${main.slug}, ${main.senses[0].english_definitions.join(", ")}`
+    await delay(i * 1000)
+
+    try {
+        const resp = await fetch(`https://jisho.org/api/v1/search/words?keyword=${slug}`)
+        const full = await resp.json()
+        const main = full.data[0]
+        return `${main.slug}, ${main.senses[0].english_definitions.join(", ")}`
+    }
+    catch (e) {
+        return ""
+    }
 }
 
-const requests = urls
+const requests = urls.map(getUrl)
 
 async function main() {
-    const resp = requests.map(req)
+    const resp = await Promise.all(requests)
 
     console.log(resp.join("\n"))
 }
