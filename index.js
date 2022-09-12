3772be766197c2de0dbf204e802ec19145b0d9fd
Luc Mcgrady <lucmcgrady@gmail.com>
2022-09-12 01:27:23 +0100

    Extracted: GetSlug

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ function delay(t = delay_ms) {
     });
 }
 
-async function getUrl(url, i) {
-    const slug = url.match(/https:\/\/jisho\.org\/search\/([%A-F0-9]+)$/)
-
-    if (!slug)
-        return
-
+async function getSlug(slug, i) {
+    
     await delay(i * 1000)
 
     try {
-        const resp = await fetch(`https://jisho.org/api/v1/search/words?keyword=${slug[1]}`)
+        const resp = await fetch(`https://jisho.org/api/v1/search/words?keyword=${slug}`)
         const full = await resp.json()
         const main = full.data[0]
         return `${main.slug}, ${main.japanese[0].reading} -> ${main.senses[0].english_definitions.join(" | ")}`
@@ -26,9 +22,18 @@ async function getUrl(url, i) {
     }
 }
 
-const requests = urls.map(getUrl)
+async function getUrl(url, i) {
+    const slug = url.match(/https:\/\/jisho\.org\/search\/([%A-F0-9]+)$/)
+
+    if (!slug)
+        return
+
+    getSlug(slug[1], i)
+}
+
 
 async function main() {
+	const requests = slugs.map(getSlug)
     const resp = await Promise.all(requests)
 
     console.log(resp.join("\n"))
