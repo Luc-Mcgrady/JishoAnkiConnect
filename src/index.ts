6bc4a2ad3989f675878b2a44633d56b5dfb4ff9c
Luc Mcgrady <lucmcgrady@gmail.com>
2022-10-02 00:00:10 +0100

    Added: File parsing

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import yargs from "yargs"
 import { loadFromArray } from "./jisho/csv"
 import { extractSlug } from "./jisho/slug"
+import fs from "fs/promises"
+
+function GetWords(slugs: string[]) {
+    slugs = slugs.map(extractSlug)
+
+    console.log(slugs)
+
+    loadFromArray(slugs.map(e=>e.toString()))
+}
 
 yargs(process.argv.splice(2))
     .scriptName("jishocsv")
     .command(
-        "$0 [word-list...]",
-        "Get csv from words",
+        "$0 <word-list...>",
+        "Get csv from words or urls",
         {
             slugs: {
                 alias: "word-list",
@@ -15,12 +24,28 @@ yargs(process.argv.splice(2))
             }
         },
         (argv)=>{
-            const slugs = argv.slugs.map(extractSlug).filter(e=>!!e)
-
-            console.log(slugs)
-
-            loadFromArray(argv.slugs.map(e=>e.toString()))
+            GetWords(argv.slugs)
         },    
+    )
+    .command(
+        "file <filename>",
+        "Get csv from words or urls locaited in a file",
+        {
+            file: {
+                alias: "filename",
+                type: "string",
+                required: true
+            }
+        },
+        async (argv)=>{
+            const file = await fs.readFile(argv.file)
+            const filestring = file.toString()
+            const wordMatches = filestring.matchAll(/(.+?)(?:[ ]|$)/gm) // Splits the words based on space
+
+            const words = [...wordMatches].map(a=>a[1])
+            console.log({file, filestring, wordMatches, words})
+            GetWords(words)
+        }
     )
     .help()
     .argv
