9e5c01bad37d66b0d77c96ced7f72514ad574438
Luc Mcgrady <lucmcgrady@gmail.com>
2022-10-01 22:39:36 +0100

    Added: Yargs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
 import yargs from "yargs"
 
-yargs
-    .scriptName("Jisho->Csv")
-    .usage('$0 <cmd> [args]')
-    .command("hi","woo",()=>{})
+yargs(process.argv.splice(2))
+    .scriptName("JishoToCsv")
+    .command("words", "Get csv from words", 
+    (yargs)=>{
+        return yargs.option("words", {
+            alias: "words",
+            type: "array",
+            describe: "A list of words to get the csv for"
+        })
+    },
+    (argv)=>{
+        console.log("To be implemented", argv.words)
+    })
+    .command("urls", "Get csv from jisho urls",
+    (yargs)=>{
+        return yargs.option("urls", {
+            alias: "urls",
+            type: "array",
+            describe: "A list of urls to get the csv for"
+        })
+    },
+    (argv)=>{
+        console.log("To be implemented", argv.urls)
+    })
+    
+    .argv
+    
