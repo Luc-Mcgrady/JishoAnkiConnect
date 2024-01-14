0ee64e91dc5d6675f81f115dc0d425386d96e958
Luc Mcgrady <lucmcgrady@gmail.com>
2024-01-14 00:27:40 +0000

    Added: buildscript

--- esbuild.mjs
+++ esbuild.mjs
@@ -2,8 +2,16 @@ import esbuild from "esbuild"
 
 await esbuild.build({
     sourceRoot: "src",
-    entryPoints: ["src/index.ts"],
+    entryPoint: "src/index.ts",
     outfile: "dist/cli.js",
     bundle: true,
     platform: "node",
+})
+
+await esbuild.build({
+    sourceRoot: "src",
+    entryPoint: "src/index.ts",
+    outfile: "dist/cli.js",
+    bundle: true,
+    platform: "commonjs",
 })
